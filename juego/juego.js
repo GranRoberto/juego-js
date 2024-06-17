5a6216970a78a4bab00ec8e53aa4682d2c485c01
GranRoberto <robertocariolo94@gmail.com>
2024-06-17 17:38:55 -0400

    a

---
+++ juego/juego.js
@@ -0,0 +1,28 @@
+/*
+  Proyectos de la sección
+  1) Utiliza programación orientada a objetos para crear un juego de peleas
+  donde existan dos personajes y se peleen entre ellos hasta que uno de los dos
+  se quede sin vida (por turnos).
+
+  un personaje tiene nombre, vida y ataque, defensa
+
+  Cada turno se debe imprimir el daño que se le hace al personaje y la vida que le queda
+  a cada uno. Al final se debe imprimir el ganador.
+
+  Los personajes deben tener un metodo que les permita atacar a otro personaje
+  y un metodo que les permita defenderse.
+
+  Deben ejecutar uno de los dos metodos de forma aleatoria.
+  Y quien ataca o defiende primero debe ser aleatorio.
+
+  La defensa lo que hace es sumar +3 de vida al personaje que se defiende
+
+  el ataque debe ser debe tener un porcentaje aleatorio de quitar el 100% del ataque
+  del personaje que ataca
+
+  2) Crea un programa que te permita jugar piedra, papel o tijera contra la computadora
+  en programación orientada a objetos, guardar un historial de victorias y derrotas
+
+  Fecha de entrega: 20/06/2024
+*/
+
