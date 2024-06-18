621fceed5cbcfafb9494e1575f39841fdd9c52a3
GranRoberto <robertocariolo94@gmail.com>
2024-06-17 21:20:49 -0400

    a

--- juego/juego.js
+++ juego/juego.js
@@ -27,7 +27,7 @@
 */
 
 class Personaje {
-  constructor (nombre, vida, ataque, defensa, velocidad) {
+  constructor (nombre, vida, ataque, defensa) {
     this.nombre = nombre;
     this.vida = vida;
     this.ataque = ataque;
@@ -40,82 +40,46 @@ class Personaje {
   ataque(oponente) {
     let daño = Math.floor(Math.random() * this.ataque) * (Math.random() < 0.5);
     oponente.recibe_daño(daño);
-    console.log(`${this.name} ataca ${opponent.name} por ${damage} de daño!`);
+    console.log(`${this.nombre} ataca a ${opponent.nombre} por ${daño} de daño!`);
   }
 
-  defend() {
-    this.health += 3;
-    console.log(`${this.name} defends themselves!`);
+  defensa() {
+    this.vida += 3;
+    console.log(`${this.nombre} se defiende asímismo!`);
   }
 
-  receiveDamage(damage) {
-    this.health -= damage;
-    console.log(`${this.name} takes ${damage} damage!`);
-  }
-}
-
-let goku = new Personaje ('Goku', 100, 5, 3);
-let vegeta = new Personaje ('Vegeta', 100, 5, 3);
-
-goku.saludar();
-vegeta.saludar();
-
-function pelear () {
-
-}
-
-pelear(goku, vegeta)
-
-
-class Character {
-  constructor(name, health, attack, defense) {
-    this.name = name;
-    this.health = health;
-    this.attack = attack;
-    this.defense = defense;
-  }
-
-  attack(opponent) {
-    const damage = Math.floor(Math.random() * this.attack) * (Math.random() < 0.5);
-    opponent.receiveDamage(damage);
-    console.log(`${this.name} attacks ${opponent.name} for ${damage} damage!`);
-  }
-
-  defend() {
-    this.health += 3;
-    console.log(`${this.name} defends themselves!`);
-  }
-
-  receiveDamage(damage) {
-    this.health -= damage;
-    console.log(`${this.name} takes ${damage} damage!`);
+  recibe_daño(daño) {
+    this.vida -= daño;
+    console.log(`${this.nombre} recive ${daño} de daño!`);
   }
 }
 
-function startFight(character1, character2) {
-  let turn = Math.random() < 0.5 ? 'attack' : 'defend';
+function pelea(goku, vegeta) {
+  let turno = Math.random() < 0.5 ? 'ataca' : 'defiende';
 
   while (true) {
-    if (turn === 'attack') {
-      const [attacker, defender] = [character1, character2].sort(() => Math.random() - 0.5);
-      attacker.attack(defender);
+    if (turno === 'ataca') {
+      let [atacante, defensor] = [goku, vegeta].sort(() => Math.random() - 0.5);
+      atacante.ataque(defensor);
 
-      if (defender.health <= 0) {
-        console.log(`${defender.name} has been defeated!`);
+      if (defensor.vida <= 0) {
+        console.log(`${defensor.nombre} fue derrotado!`);
         break;
       }
 
-      turn = 'defend';
+      turno = 'defiende';
     } else {
-      const [defender, attacker] = [character1, character2].sort(() => Math.random() - 0.5);
-      defender.defend();
-      turn = 'attack';
+      let [defensor, atacante] = [goku, vegeta].sort(() => Math.random() - 0.5);
+      defensor.defensa(atacante);
+      turno = 'ataca';
     }
   }
 }
 
-const character1 = new Character('Player 1', 100, 20, 10);
-const character2 = new Character('Player 2', 100, 15, 15);
+let goku = new Personaje('Goku', 100, 20);
+let vegeta = new Personaje('Vegeta', 100, 15);
 
-console.log('**Fight Begins!**');
-startFight(character1, character2);
+goku.saludar();
+vegeta.saludar();
+console.log('Que comience la pelea!');
+pelea(goku, vegeta);
